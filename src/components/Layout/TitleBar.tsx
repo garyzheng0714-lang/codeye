7e34ecf3b2bdc3f7716f7fba783249f64e178929
simba_gary <simba@Mac-mini.local>
2026-03-14 12:43:23 +0800

    style: remove titlebar breadcrumb text next to traffic lights

--- src/components/Layout/TitleBar.tsx
+++ src/components/Layout/TitleBar.tsx
@@ -1,26 +1,11 @@
-import { useChatStore } from '../../stores/chatStore';
 import GitActionMenu from './GitActionMenu';
 import ModeToggle from './ModeToggle';
 
 export default function TitleBar() {
-  const cwd = useChatStore((s) => s.cwd);
-  const breadcrumbParts = cwd ? cwd.split('/').filter(Boolean).slice(-2) : [];
-
   return (
     <div className="title-bar">
       <div className="title-bar-content">
-        <div className="title-bar-left">
-          {breadcrumbParts.length > 0 && (
-            <div className="title-bar-breadcrumb">
-              {breadcrumbParts.map((part, i) => (
-                <span key={`${i}-${part}`}>
-                  {i > 0 && <span className="sep"> / </span>}
-                  {part}
-                </span>
-              ))}
-            </div>
-          )}
-        </div>
+        <div className="title-bar-left" />
         <div className="title-bar-center">
           <ModeToggle />
         </div>
